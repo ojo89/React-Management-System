528bd23bf23f032ab572b7be5437c96c7a12e920
禹 宗完 <jo.woo@widetec.com>
2020-04-02 10:58:37 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
-import logo from './404_Img_cyan.png';
+import Customer from './components/Customer';
 import './App.css';
+
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': 'ab',
+    'birthday': '200420',
+    'gender': 'boy',
+    'job': 'student'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': 'test',
+    'birthday': '200320',
+    'gender': 'man',
+    'job': 'teacher'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': 'polestar',
+    'birthday': '200321',
+    'gender': 'girl',
+    'job': 'banker'
+  }
+]
+
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>ADD on Tool</h2>
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+              );
+          })
+         }
       </div>
     );
   }
