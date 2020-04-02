78d2e7b22ab5f9d5cfbcaf0b5f77ba919d61878c
禹 宗完 <jo.woo@widetec.com>
2020-04-02 10:08:18 +0900

    Creat React Project

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## ADD on TOOL
+Test用プロジェクト
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './404_Img_cyan.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>ADD on Tool</h1>
-        </p>
-        <a
-          className="App-link"
-          href="http://192.168.10.186:8080"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          POLESTAR Automation V3
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="gray-background">
+        <img src={logo} lat="logo" />
+        <h2>ADD on Tool</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
