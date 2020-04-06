107b5c9acf205f2ef5e9329f9fd23d842d0796f8
禹 宗完 <jo.woo@widetec.com>
2020-04-06 10:45:07 +0900

    Create Customer Database Table & Node.js Database Connection Setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-          'id': 1,
-          'image': 'https://placeimg.com/64/64/1',
-          'name': 'ab',
-          'birthday': '200420',
-          'gender': 'boy',
-          'job': 'student'
-        },
-        {
-          'id': 2,
-          'image': 'https://placeimg.com/64/64/2',
-          'name': 'test',
-          'birthday': '200320',
-          'gender': 'man',
-          'job': 'teacher'
-        },
-        {
-          'id': 3,
-          'image': 'https://placeimg.com/64/64/3',
-          'name': 'polestar',
-          'birthday': '200321',
-          'gender': 'girl',
-          'job': 'banker'
-        }
-    ]);
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
